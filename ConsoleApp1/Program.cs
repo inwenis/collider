f2ebc8416b324c6a14779e1b535e41cd3da33c7d
Filip Kucharczyk <fku@danskecommodities.com>
2020-06-13 10:28:36 +0200

    refactor

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -24,7 +24,7 @@ namespace ConsoleApp1
             File.WriteAllText("out2.csv", top + csv);
 
             var allLines = File.ReadAllLines("out2.csv");
-            Read(allLines, out List<Particle> particles2, out Options options2);
+            ParseCsv(allLines, out IEnumerable<Particle> particles2, out Options options2);
 
             csv = ToCsvFixedWidth(particles2.ToList());
             File.WriteAllText("out3.csv", csv);
@@ -33,51 +33,67 @@ namespace ConsoleApp1
             Console.ReadKey();
         }
 
-        private static string ToCsvHeaders(Options op)
+        private static string ToCsvHeaders(Options o)
         {
-            var s = $"{nameof(op.NumberOfFrames)}={op.NumberOfFrames}\n" +
-                    $"size={string.Join(",", op.Dimensions)}\n";
+            // Only NumberOfFrame and size matter when reading options from a file.
+            // Dimensions are called 'size' for user's convenience
+            var s = $"{nameof(o.NumberOfFrames)}={o.NumberOfFrames}\n" +
+                    $"size={string.Join(",", o.Dimensions)}\n";
             return s;
         }
 
-        private static void Read(string[] allLines, out List<Particle> particles, out Options options)
+        private static Options ParseFromCsvHeaders(string[] lines)
         {
-            var header = allLines[2];
-            var split = header.Split('|');
-            var headers = split.Select(x => x.Replace(" ", ""));
-            var rows = new List<Dictionary<string, string>>();
-            foreach (var row in allLines.Skip(3))
-            {
-                split = row.Split('|');
-                var values = split.Select(x => x.Replace(" ", ""));
-                var rowAsDict = headers.Zip(values, (s, s1) => (s, s1)).ToDictionary(x => x.s, x => x.s1);
-                rows.Add(rowAsDict);
-            }
-
-            particles = rows.Select(x => ToParticle(x)).ToList();
-
-            var n = allLines[0].Split('=')[1];
-            var dims = allLines[1].Split('=')[1].Split(',');
-            options = new Options
+            // expected format:
+            // NumberOfFrames=1000
+            // size = 800,800
+            // ...
+            var n = lines[0].Split('=')[1];
+            var s = lines[1].Split('=')[1].Split(',');
+            return new Options
             {
                 NumberOfFrames = int.Parse(n),
-                Dimensions = dims.Select(x => int.Parse(x))
+                Dimensions = s.Select(x => int.Parse(x))
             };
         }
 
-        private static Particle ToParticle(Dictionary<string, string> dictionary)
+
+        private static void ParseCsv(string[] lines, out IEnumerable<Particle> particles, out Options options)
         {
-            var posX = float.Parse(dictionary["positionX"]);
-            var posY = float.Parse(dictionary["positionY"]);
-            var velX = float.Parse(dictionary["velocityX"]);
-            var velY = float.Parse(dictionary["velocityY"]);
-            var sig = int.Parse(dictionary["radius"]);
-            return new Particle
+            particles = ParseParticles(lines);
+            options = ParseFromCsvHeaders(lines);
+        }
+
+        private static IEnumerable<Particle> ParseParticles(string[] lines)
+        {
+            var headers = lines[2].Split('|').Select(x => x.Trim()).ToArray();
+            var particles = lines
+                .Skip(3)
+                .Select(x => RowToDict(x))
+                .Select(x => DictToParticle(x));
+            return particles;
+
+            Dictionary<string, string> RowToDict(string row)
             {
-                Pos = new Vector2(posX, posY),
-                Vel = new Vector2(velX, velY),
-                Sig = sig
-            };
+                var values = row.Split('|').Select(x => x.Trim());
+                var rowAsDict = headers.Zip(values, (h, v) => (h, v)).ToDictionary(x => x.h, x => x.v);
+                return rowAsDict;
+            }
+
+            Particle DictToParticle(Dictionary<string, string> dictionary)
+            {
+                var posX = float.Parse(dictionary["positionX"]);
+                var posY = float.Parse(dictionary["positionY"]);
+                var velX = float.Parse(dictionary["velocityX"]);
+                var velY = float.Parse(dictionary["velocityY"]);
+                var sig = int.Parse(dictionary["radius"]);
+                return new Particle
+                {
+                    Pos = new Vector2(posX, posY),
+                    Vel = new Vector2(velX, velY),
+                    Sig = sig
+                };
+            }
         }
 
         private static string MagicFormat(float f)
