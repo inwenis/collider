09074365d39dfe485412ad218a468d1c433b2eb7
Filip Kucharczyk <fku@danskecommodities.com>
2020-06-16 18:06:32 +0200

    print message if user want to generate more particles than can actually fit

--- WindowsFormsApp1/ParticlesGenerator.cs
+++ WindowsFormsApp1/ParticlesGenerator.cs
@@ -10,6 +10,7 @@ namespace WindowsFormsApp1
     {
         public static void AddRandomParticles(List<Particle> existingParticles, int count, int sig, float mass, Size size)
         {
+            ValidateInputParameters(count, sig, size);
             var random = new Random(DateTimeOffset.UtcNow.Millisecond);
             for (int i = 0; i < count; i++)
             {
@@ -21,6 +22,26 @@ namespace WindowsFormsApp1
                     Mass = mass
                 };
                 existingParticles.Add(particle);
+            }
+        }
+
+        private static void ValidateInputParameters(int count, int sig, Size size)
+        {
+            // check if particles can fit into given space without overlapping
+            var xn = size.Width / (2 * sig); // max particles in x axis
+            var yn = size.Height / (2 * sig); // max particles in y axis
+            var max = xn * yn; // max particles in space of given size
+            var saturation = (double) count / max;
+            // threshold below were checked empirically
+            if (saturation > 0.7)
+            {
+                throw new Exception($"You have requested {count} particles in space {size}. Requested density of particles is very high and randomly generated particles might not fit or randomization will take tens of minutes.");
+            }
+            else if (saturation > 0.6)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"WARNING - you have requested {count} particles in space {size} (saturation={saturation}). Requested density of particles is high and randomly generated particles might not fit or randomization will take minutes.");
+                Console.ResetColor();
             }
         }
 
