8196a8b7cd2befc76fd3ed35f0c9f2e351c92dd6
Filip Kucharczyk <fku@danskecommodities.com>
2020-06-21 21:55:00 +0200

    testing performance

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -10,10 +11,11 @@ namespace Tests
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var inputCsv = "input.csv";
             var nFrames = 6000;
+            int nPartic = 200;
             var size = new Size(700, 400);
 
             List<Particle> particles = null;
@@ -25,12 +27,43 @@ namespace Tests
             else
             {
                 particles = new List<Particle>();
-                ParticlesGenerator.AddRandomParticles(particles, 20, 5, 1, size);
-                var options = new Options(){NumberOfFrames = 20, Dimensions = new []{ 700, 400}};
+                ParticlesGenerator.AddRandomParticles(particles, nPartic, 5, 1, size);
+                var options = new Options(){NumberOfFrames = nFrames, Dimensions = new []{ size.Width, size.Height}};
                 var serializedToCsv = CsvSerializer.ToCsvFixedWidth(options, particles);
                 File.WriteAllText($"{DateTime.Now:yyyy-MM-dd--HH-mm-ss}.csv", serializedToCsv);
             }
 
+            //CompareWorkers(particles, nFrames, size);
+            MeasureTime(particles, nFrames, size);
+        }
+
+        private static void MeasureTime(List<Particle> particles, int nFrames, Size size)
+        {
+            var w = new WorkerArray();
+            List<Particle[]> frames;
+
+            // warmup x5
+            for (int i = 0; i < 5; i++)
+            {
+                frames = w.Simulate(particles, size).Take(nFrames).ToList();
+            }
+
+            // test x10
+            var results = new List<TimeSpan>();
+            for (int i = 0; i < 10; i++)
+            {
+                var sw = Stopwatch.StartNew();
+                frames = w.Simulate(particles, size).Take(nFrames).ToList();
+                results.Add(sw.Elapsed);
+            }
+
+            var average = results.Average(x => x.TotalMilliseconds);
+            Console.WriteLine(TimeSpan.FromMilliseconds(average));
+
+        }
+
+        private static void CompareWorkers(List<Particle> particles, int nFrames, Size size)
+        {
             var wA = new Worker();
             var wB = new WorkerArray();
 
@@ -50,11 +83,11 @@ namespace Tests
             {
                 Console.WriteLine("No diff");
             }
-            
-            //foreach (var framesComparison in framesComparisons.OrderBy(x => x.Key).Select(x => x.Value))
-            //{
-            //    Console.WriteLine($"{framesComparison.TotalDiff}");
-            //}
+
+            foreach (var framesComparison in framesComparisons.OrderBy(x => x.Key).Select(x => x.Value))
+            {
+                Console.WriteLine($"{framesComparison.TotalDiff}");
+            }
         }
     }
 }
