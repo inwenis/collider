82af000122c06369df787fae3d62ccbc7397da6a
Filip Kucharczyk <fku@danskecommodities.com>
2020-07-03 07:24:09 +0200

    support consle args for testing app

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -6,35 +6,49 @@ using System.IO;
 using System.Linq;
 using WindowsFormsApp1;
 using WindowsFormsApp1.Csv;
+using CommandLine;
 
 namespace Tests
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            var inputCsv = "input.csv";
-            var nFrames = 6000;
-            int nPartic = 400;
-            var size = new Size(700, 400);
+            var parserResult = Parser.Default.ParseArguments<Options>(args);
+            parserResult.WithParsed(RunApp);
+        }
 
-            List<Particle> particles = null;
-            if (File.Exists(inputCsv))
+        static void RunApp(Options options)
+        {
+            Size size;
+
+            List<Particle> particles;
+            if (options.ParticlesFile != null)
             {
-                CsvSerializer.ParseCsv(File.ReadAllLines(inputCsv), out var options, out var outParticles);
+                var lines = File.ReadAllLines(options.ParticlesFile);
+                // not super happy about this code cuz we overwrite options here
+                CsvSerializer.ParseCsv(lines, out options, out var outParticles);
                 particles = outParticles.ToList();
+
+                var dimensions = options.Dimensions.ToArray();
+                size = new Size(dimensions[0], dimensions[1]);
             }
             else
             {
+                var dimensions = options.Dimensions.ToArray();
+                size = new Size(dimensions[0], dimensions[1]);
+
                 particles = new List<Particle>();
-                ParticlesGenerator.AddRandomParticles(particles, nPartic, 5, 1, size);
-                var options = new Options(){NumberOfFrames = nFrames, Dimensions = new []{ size.Width, size.Height}};
+                ParticlesGenerator.AddRandomParticles(particles, options.NumberOfParticles, options.Radius, 1, size);
+
                 var serializedToCsv = CsvSerializer.ToCsvFixedWidth(options, particles);
-                File.WriteAllText($"{DateTime.Now:yyyy-MM-dd--HH-mm-ss}.csv", serializedToCsv);
+                var fileName = $"{DateTime.Now:yyyy-MM-dd--HH-mm-ss}.csv";
+                File.WriteAllText(fileName, serializedToCsv);
+                Console.WriteLine($"Particles saved to {fileName}. To rerun use: --file={fileName}");
             }
 
             //CompareWorkers(particles, nFrames, size);
-            MeasureTime(particles, nFrames, size);
+            MeasureTime(particles, options.NumberOfFrames, size);
         }
 
         private static void MeasureTime(List<Particle> particles, int nFrames, Size size)
